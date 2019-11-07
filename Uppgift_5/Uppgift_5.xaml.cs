25f506f7fe818534fc151c87d669d4805a93b651
smith.n.victor@gmail.com <smith.n.victor@gmail.com>
2019-11-07 19:59:43 +0100

    Uppgift 6

---
+++ Uppgift_5/Uppgift_5.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Uppgift_5
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void BtnClick(object sender, RoutedEventArgs e)
+        {
+            if (Convert.ToString((sender as Button).Content) != "Rensa")
+            {
+                if (!int.TryParse(InputOne.Text, out int i) || !int.TryParse(InputTwo.Text, out int k)) {
+                       MessageBox.Show("Använd bara siffror tack.");
+                        InputOne.Clear();
+                        InputTwo.Clear();
+                        TotalSum.Clear();
+                } else { 
+                    string a, b;
+                    int c;
+                        a = InputOne.Text;
+                        b = InputTwo.Text;
+                        c = Convert.ToInt32(a) + Convert.ToInt32(b);
+                            TotalSum.Text = Convert.ToString(c);
+                }
+            } else
+            {
+                InputOne.Clear();
+                InputTwo.Clear();
+                TotalSum.Clear();
+            }
+        }
+    }
+}
---
+++ Uppgift_6/Uppgift_6.xaml.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Uppgift_6
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ClearAllForm()
+        {
+            InputOne.Clear();
+            InputTwo.Clear();
+            SumTotal.Clear();
+        }
+
+        private void BtnClick(object sender, RoutedEventArgs e)
+        {
+            if(Convert.ToString((sender as Button).Content) != "Rensa")
+            {
+                double a, b, c;
+                char f;
+
+                a = Convert.ToDouble(InputOne.Text);
+                b = Convert.ToDouble(InputTwo.Text);
+
+                f = Convert.ToChar((sender as Button).Content);
+                if (InputOne.Text.Contains(',') || InputTwo.Text.Contains(','))
+                {
+                    MessageBox.Show("Använd inte komma, använd punkt.");
+                    ClearAllForm();
+                }
+                else
+                {
+                    if (f == '+')
+                    {
+                        c = Math.Round((a + b), 2);
+                        SumTotal.Text = Convert.ToString(c);
+
+                    }
+                    if (f == '-')
+                    {
+                        c = Math.Round((a - b), 2);
+                        SumTotal.Text = Convert.ToString(c);
+                    }
+                    if (f == '/')
+                    {
+                        c = Math.Round((a / b), 2);
+                        SumTotal.Text = Convert.ToString(c);
+                    }
+                    if (f == '*')
+                    {
+                        c = Math.Round((a * b), 2);
+                        SumTotal.Text = Convert.ToString(c);
+                    }
+
+
+                }
+                
+
+            }
+            else
+            {
+                ClearAllForm();
+            }
+
+            }
+        }
+    }
+
