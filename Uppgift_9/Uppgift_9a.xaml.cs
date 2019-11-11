0bcc5991024c58363439f7a4ed1b91546114d447
smith.n.victor@gmail.com <smith.n.victor@gmail.com>
2019-11-11 17:47:20 +0100

    Uppgift 9

---
+++ Uppgift_9/Uppgift_9a.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Uppgift_9a
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnClk(object sender, RoutedEventArgs e)
+        {
+            int ageInput;
+
+            ageInput = int.Parse(Age.Text);
+            
+            if (ageInput >= 16)
+            {
+                Explanation.Content = "Hej " + Name.Text + "! Du är " + ageInput + " och du får se alla filmer.";
+            }
+            else if (ageInput == 15)
+            {
+                Explanation.Content = "Hej " + Name.Text + "! Du är " + ageInput + " och du får se alla filmer om du kan styrka detta.";
+            }
+            else if (ageInput >= 11 && ageInput <= 14)
+            {
+                Explanation.Content = "Hej " + Name.Text + "! Du är " + ageInput + " och du får se alla filmer upp till åldersgränsen på 11 år.";
+            }
+            else if (ageInput >= 7 && ageInput <= 10)
+            {
+                Explanation.Content = "Hej " + Name.Text + "! Du är " + ageInput + " och du får se alla filmer upp till åldersgränsen på 11 år om du är i vuxet sällskap.";
+            }
+            else if (ageInput < 7)
+            {
+                Explanation.Content = "Hej " + Name.Text + "! Du är " + ageInput + " och du får se alla barntillåtna filmer.";
+            }
+        }
+
+    }
+}
---
+++ Uppgift_9b/Uppgift_9b.xaml.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Uppgift_9b
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private bool adult;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void WithAdult_Checked(object sender, RoutedEventArgs e)
+        {
+            adult = true;
+        }
+        private void WithoutAdult_Checked(object sender, RoutedEventArgs e)
+        {
+            adult = false;
+        }
+
+        private void BtnClk(object sender, RoutedEventArgs e)
+        {
+            int ageInput;
+
+            ageInput = int.Parse(Age.Text);
+
+            if (ageInput >= 16)
+            {
+                Explanation.Content = "Hej " + Name.Text + "! Du är " + ageInput + " och du får se alla filmer.";
+            }
+            else if (ageInput == 15)
+            {
+                Explanation.Content = "Hej " + Name.Text + "! Du är " + ageInput + " och du får se alla filmer om du kan styrka din ålder.";
+            }
+
+            if (ageInput <= 15 && adult)
+            {
+                if (ageInput >= 7 && ageInput <= 14)
+                {
+                    Explanation.Content = "Hej " + Name.Text + "! Du är " + ageInput + " och du får se alla filmer upp till åldersgränsen på 11 år eftersom du är i vuxet sällskap.";
+                }
+                else if (ageInput > 0 && ageInput < 7)
+                {
+                    Explanation.Content = "Hej " + Name.Text + "! Du är " + ageInput + " och du får se alla filmer upp till åldersgränsen på 7 år eftersom du är i vuxet sällskap.";
+                }
+                else
+                {
+                    Explanation.Content = "Skriv in en riktig ålder.";
+                }
+            } 
+
+            else if (ageInput <= 15 && !adult)
+            {
+                if (ageInput >= 11 && ageInput <= 14)
+                {
+                    Explanation.Content = "Hej " + Name.Text + "! Du är " + ageInput + " och du får se alla filmer med åldergräns på 11 år.";
+                }
+                else if (ageInput >= 7 && ageInput <= 10)
+                {
+                    Explanation.Content = "Hej " + Name.Text + "! Du är " + ageInput + " och du får se alla filmer med åldergräns på 7 år.";
+                }
+                else if (ageInput > 0 && ageInput < 7)
+                {
+                    Explanation.Content = "Hej " + Name.Text + "! Du är " + ageInput + " och du får se alla barntillåtna filmer.";
+                }
+                else
+                {
+                    Explanation.Content = "Skriv in en riktig ålder.";
+                }
+            }
+        }
+    }
+}
