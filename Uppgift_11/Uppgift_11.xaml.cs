ce3110e6a7e1e69915ddaa0a04700994924843e5
smith.n.victor@gmail.com <smith.n.victor@gmail.com>
2019-11-12 16:14:57 +0100

    Uppgift 11

--- Uppgift_11/Uppgift_11.xaml.cs
+++ Uppgift_11/Uppgift_11.xaml.cs
@@ -20,9 +20,61 @@ namespace Uppgift_11
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        int luck, flips, success, fails, edge;
+        Random flip = new Random();
+
+        private void ResultUpdate(int s, int f, int e)
+        {
+            InfoResult.Text = 
+                "Antal åt rätt håll: " + s + Environment.NewLine + 
+                "Antal åt fel håll: " + f + Environment.NewLine + 
+                "Antal som hamnade på kanten: " + e;
+        }
+
         public MainWindow()
         {
             InitializeComponent();
+            ResultUpdate(0,0,0);
+        }
+
+        private void Luck_Click(object sender, RoutedEventArgs e)
+        {
+            if ((String)(sender as Button).Content == "Mer Tur") {
+                VisualLuck.Value += 5;
+                Lucky.Content = VisualLuck.Value + "%";
+                Unlucky.Content = 100 - VisualLuck.Value + "%";
+            }
+            else
+            {
+                VisualLuck.Value -= 5;
+                Lucky.Content = VisualLuck.Value + "%";
+                Unlucky.Content = 100 - VisualLuck.Value + "%";
+            }
+        }
+
+        private void Start_Click(object sender, RoutedEventArgs e)
+        {
+            luck = Convert.ToInt32(VisualLuck.Value);
+
+            for (int t = (Convert.ToInt32(NumberTries.Text)); t > 0; t--)
+            {
+                flips = flip.Next(100);
+                if (luck > flips)
+                {
+                    success++;
+                } else if (luck < flips)
+                {
+                    fails++;
+                }else if (luck == flips)
+                {
+                    edge++;
+                }
+            }
+            ResultUpdate(success, fails, edge);
+            success = 0;
+            fails = 0;
+            edge = 0;
         }
     }
 }
